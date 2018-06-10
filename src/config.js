d5e91735b8026dd410a5f4afb6b1eba05a465df4
Isaias Francisco dos Santos <isaias.fran@gmail.com>
2018-06-10 20:47:42 -0300

    Deletar posts e tipo blog dos posts

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL =  process.env.API_URL || 'http://localhost:3001'  //'https://api.isaiasfrancisco.com.br' 
-const DOMAIN = process.env.DOMAIL || 'http://localhost:3001' //'http://api.isaiasfrancisco.com.br'
+const DOMAIN = process.env.DOMAIL || 'http://api.isaiasfrancisco.com.br'
 export default {
     API_URL,
     DOMAIN
