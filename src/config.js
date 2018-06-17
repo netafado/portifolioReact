aa1085ba780124e89ffbba54d9b53d40ce3379ef
Isaias Francisco dos Santos <isaias.fran@gmail.com>
2018-06-17 09:07:56 -0300

    alter

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const API_URL =  process.env.API_URL ||  'http://localhost:3001' //'https://api.isaiasfrancisco.com.br' 
-const DOMAIN = process.env.DOMAIL || 'http://localhost:3001' //'http://api.isaiasfrancisco.com.br'
+const API_URL =  process.env.API_URL ||  'https://api.isaiasfrancisco.com.br' //'http://localhost:3001' 
+const DOMAIN = process.env.DOMAIL || 'http://api.isaiasfrancisco.com.br' //'http://localhost:3001' 
 console.log(`${API_URL} and ${DOMAIN}`)
 export default {
     API_URL,
