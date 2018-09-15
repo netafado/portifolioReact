1964340e2969bbebde95dff1f8c7bb87cb460839
netafado <isaias.fran@gmail.com>
2018-09-15 19:17:06 -0300

    alter

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const API_URL =  process.env.API_URL || 'https://api.isaiasfrancisco.com.br'  /*'http://localhost:3001'*/  
-const DOMAIN = process.env.DOMAIL ||  'https://api.isaiasfrancisco.com.br'   /*'http://localhost:3001'*/ 
+const API_URL =  process.env.API_URL || /* 'http://localhost:3001'*/  'https://api.isaiasfrancisco.com.br'  
+const DOMAIN = process.env.DOMAIL ||  /*'http://localhost:3001'*/   'https://api.isaiasfrancisco.com.br' 
 
 export default {
     API_URL,
--- src/containers/BlogArtigo.jsx
+++ src/containers/BlogArtigo.jsx
@@ -5,6 +5,7 @@ import Loader from '../utils/carregando'
 import { connect } from 'react-redux'
 import config from '../config'
 import {getPost} from '../actions/index'
+import { Link } from 'react-router-dom'
 
 class BlogArtigo extends Component{
     state = {
@@ -19,6 +20,11 @@ class BlogArtigo extends Component{
         console.log("will mount");
         const id = this.props.match.params.id
         this.props.dispatch(getPost(id));
+    }
+
+    onclickBeforeAffter(){
+        const id = this.props.match.params.id
+        this.props.dispatch(getPost(id));       
     }
 
 
@@ -41,10 +47,33 @@ class BlogArtigo extends Component{
                 return(
                     <div>
                         <div className="img-wrapper">                            
-                            <img className="img-responsive" src={`${config.DOMAIN}/upload/${this.props.post.img}`} onLoad={this.imgLoading.bind(this)} alt="imagem principal" />
+                            <img className="img-responsive" src={`${config.DOMAIN}/upload/${this.props.post.current.img}`} onLoad={this.imgLoading.bind(this)} alt="imagem principal" />
                         </div> 
-                        <div className="conteudo" dangerouslySetInnerHTML={{__html: this.props.post.content}}>
+                        <div className="conteudo" dangerouslySetInnerHTML={{__html: this.props.post.current.content}}>
                         </div>
+                        
+                        <div className="row">
+                            <div className="col-sm-12">
+                                {this.props.prevpost || this.props.nextpost ? <hr /> : null}
+                                {this.props.post.prevpost ? 
+                                    <Link to={`/post/${this.props.post.prevpost._id}`}  onClick={this.onclickBeforeAffter}><p className="text-left">Anterior</p></Link>
+                                    :
+                                    null
+                                }
+                                {this.props.post.nextpost ? 
+                                    <Link to={`/post/${this.props.post.nextpost._id}`} onClick={this.onclickBeforeAffter}><p className="text-right">Proximo</p></Link>
+                                    :
+                                    null
+                                }
+                            </div>                        
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <Link to="/portifolio">Voltar para Portifolio</Link>
+                            </div>                            
+                        </div>
+                        
                     </div>
                 );
             break;
@@ -67,6 +96,14 @@ class BlogArtigo extends Component{
                         </div> 
                         <div className="conteudo" dangerouslySetInnerHTML={{__html: this.props.post.content}}>
                         </div>
+                        {this.props.post.prevpost ? 
+                            <h1>Tem post</h1>
+                            :
+                            console.log('não tem')
+                        }
+                        <div className="row">
+                             <Link to="/portifolio">Voltar para Blog</Link>
+                        </div>
                     </div>
                 );
             break;
@@ -75,15 +112,16 @@ class BlogArtigo extends Component{
     render(){
         
         window.scrollTo(0, 0);
+        console.log(this.props);
         console.log(this.props.post ? this.props.post.type :  " ")
         return(
             <Layout >
                 <PageAnimation type="fade">
-                    { this.props.post && !this.state.carregando ?                    
+                    { this.props.post && this.props.post.current && !this.state.carregando ?                    
                         <div className="page-blog container">
-                            <h1>{this.props.post.title}</h1>
+                            <h1>{this.props.post.current.title}</h1>
                             {
-                                this.template(this.props.post.type)
+                                this.template(this.props.post.current.type)
                             }                  
                         </div>                
                     :
